0b2921b20cf4e6067ae70d567b2caee570e612e1
Andrew Grosser <dioptre@gmail.com>
2026-01-08 00:04:37 -0800

    preparing for deploy
    Signed-off-by: Andrew Grosser <dioptre@gmail.com>

--- sing2midi/vite.config.js
+++ sing2midi/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tidal/',
   plugins: [
     react({
       include: '**/*.{jsx,js}',
@@ -10,14 +11,21 @@ export default defineConfig({
   resolve: {
     alias: {
       'react-native': 'react-native-web',
+      buffer: 'buffer',
     },
     extensions: ['.web.js', '.js', '.web.jsx', '.jsx', '.json'],
+  },
+  define: {
+    global: 'globalThis',
   },
   optimizeDeps: {
     esbuildOptions: {
       resolveExtensions: ['.web.js', '.js', '.web.jsx', '.jsx'],
       loader: {
         '.js': 'jsx',
+      },
+      define: {
+        global: 'globalThis',
       },
     },
   },
